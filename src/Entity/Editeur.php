8d2adf142a4845186b552b0f2c51defe8b4b547b
morgan <chrysgan@gmail.com>
2026-03-15 09:37:18 +0100

    Creation Entité Editeur sans Relation Pas de migration

---
+++ src/Entity/Editeur.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EditeurRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EditeurRepository::class)]
+class Editeur
+{
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $idediteur = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $anneeCreation = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $anneeFermeture = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $commentaire = null;
+
+    public function getIdediteur(): ?int
+    {
+        return $this->idediteur;
+    }
+
+    public function setIdediteur(int $idediteur): static
+    {
+        $this->idediteur = $idediteur;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getAnneeCreation(): ?int
+    {
+        return $this->anneeCreation;
+    }
+
+    public function setAnneeCreation(?int $anneeCreation): static
+    {
+        $this->anneeCreation = $anneeCreation;
+
+        return $this;
+    }
+
+    public function getAnneeFermeture(): ?int
+    {
+        return $this->anneeFermeture;
+    }
+
+    public function setAnneeFermeture(?int $anneeFermeture): static
+    {
+        $this->anneeFermeture = $anneeFermeture;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+}
